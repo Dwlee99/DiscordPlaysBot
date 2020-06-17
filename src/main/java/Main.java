a9d9e797d17feb9f055ad2ea79d5ac38605e1333
Derek Lee (Dwl72) <dwl72@cornell.edu>
2020-06-17 04:38:02 -0400

    Removed unnecessary code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,17 +7,9 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class Main extends ListenerAdapter {
 
-    private static JDA jda;
-
     public static void main(String[] args) throws javax.security.auth.login.LoginException {
         JDABuilder builder = JDABuilder.createDefault(args[0]);
-        jda = builder.build();
+        JDA jda = builder.build();
         jda.addEventListener(new MessageHandler(jda));
-
-
-    }
-
-    public static JDA getJda() {
-        return jda;
     }
 }
