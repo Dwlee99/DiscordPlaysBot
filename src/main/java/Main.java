0aff93955b6bdd269c405c93050dbd77060d026d
Derek Lee (Dwl72) <dwl72@cornell.edu>
2020-06-17 14:28:42 -0400

    Activated checking the queue when the bot begins running

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 import MessageHandling.MessageHandler;
-import gamepad.Gamepad;
+import commands.QueuePressCommand;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -10,6 +10,15 @@ public class Main extends ListenerAdapter {
     public static void main(String[] args) throws javax.security.auth.login.LoginException {
         JDABuilder builder = JDABuilder.createDefault(args[0]);
         JDA jda = builder.build();
-        jda.addEventListener(new MessageHandler(jda));
+        jda.addEventListener(new MessageHandler());
+
+        startParallelThreads();
+    }
+
+    /**
+     * Starts threads in the bot that need to be running constantly
+     */
+    private static void startParallelThreads(){
+        QueuePressCommand.startCheckingQueue();
     }
 }
