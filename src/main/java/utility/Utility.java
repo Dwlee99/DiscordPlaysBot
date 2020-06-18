5a469896979de3ba502aad9788801f9b29f6e2c0
Derek Lee (Dwl72) <dwl72@cornell.edu>
2020-06-18 13:57:34 -0400

    Send commands return the id of the message they sent

--- src/main/java/utility/Utility.java
+++ src/main/java/utility/Utility.java
@@ -1,7 +1,9 @@
 package utility;
 
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.requests.RestAction;
 
 public class Utility {
 
@@ -9,13 +11,19 @@ public class Utility {
      * Sends a message in a discord channel.
      * @param text the text content of the message
      * @param channel the channel to send the message in
+     * @return the id of the message sent
      */
-    public static void send(String text, TextChannel channel){
-        channel.sendMessage(text).queue();
+    public static long send(String text, TextChannel channel){
+        RestAction<Message> ra = channel.sendMessage(text);
+        Message message = ra.complete();
+        return message.getIdLong();
+
     }
 
-    public static void sendEmbed(MessageEmbed embed, TextChannel channel){
-        channel.sendMessage(embed).queue();
+    public static long sendEmbed(MessageEmbed embed, TextChannel channel){
+        RestAction<Message> ra = channel.sendMessage(embed);
+        Message message = ra.complete();
+        return message.getIdLong();
     }
 
 }
