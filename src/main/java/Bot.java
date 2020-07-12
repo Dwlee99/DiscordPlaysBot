9a74893d26348b1df4c7a580c3c552771cb23af3
Derek Lee (Dwl72) <dwl72@cornell.edu>
2020-07-12 00:20:43 -0400

    Very very basic GUI created (but not functional)

---
+++ src/main/java/Bot.java
@@ -0,0 +1,52 @@
+import handlers.MessageHandler;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
+import handlers.ReactionHandler;
+import commands.QueuePressCommand;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+
+import static handlers.MessageHandler.HOST;
+import static handlers.MessageHandler.PLAYER;
+
+
+public class Bot extends ListenerAdapter {
+
+    public static void startBot(String token) throws javax.security.auth.login.LoginException, InterruptedException {
+        JDABuilder builder = JDABuilder.createDefault(token);
+        JDA jda = builder.build().awaitReady();
+
+        checkRoles(jda.getGuilds());
+        jda.addEventListener(new ListenerAdapter() {
+            @Override
+            public void onGuildJoin(@Nonnull GuildJoinEvent event) { checkRoles(event.getJDA().getGuilds());
+            }
+        });
+        jda.addEventListener(new MessageHandler());
+        jda.addEventListener(new ReactionHandler());
+
+        startParallelThreads();
+    }
+
+    /**
+     * Starts threads in the bot that need to be running constantly
+     */
+    private static void startParallelThreads(){
+        QueuePressCommand.startCheckingQueue();
+    }
+
+    private static void checkRoles(List<Guild> guildList) {
+        for (Guild guild : guildList){
+            if(guild.getRolesByName(PLAYER, false).size() == 0) {
+                guild.createRole().setName(PLAYER).queue();
+            }
+            if(guild.getRolesByName(HOST, false).size() == 0) {
+                guild.createRole().setName(HOST).queue();
+            }
+        }
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,52 +1,80 @@
-import handlers.MessageHandler;
-import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
-import handlers.ReactionHandler;
-import commands.QueuePressCommand;
-import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-import javax.annotation.Nonnull;
-import java.util.List;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
 
-import static handlers.MessageHandler.HOST;
-import static handlers.MessageHandler.PLAYER;
-
-
-public class Main extends ListenerAdapter {
-
-    public static void main(String[] args) throws javax.security.auth.login.LoginException, InterruptedException {
-        JDABuilder builder = JDABuilder.createDefault(args[0]);
-        JDA jda = builder.build().awaitReady();
-
-        checkRoles(jda.getGuilds());
-        jda.addEventListener(new ListenerAdapter() {
-            @Override
-            public void onGuildJoin(@Nonnull GuildJoinEvent event) { checkRoles(event.getJDA().getGuilds());
-            }
-        });
-        jda.addEventListener(new MessageHandler());
-        jda.addEventListener(new ReactionHandler());
-
-        startParallelThreads();
-    }
+/**
+ * This class is the GUI application.
+ */
+public class Main extends Application {
 
     /**
-     * Starts threads in the bot that need to be running constantly
+     * launches the program
+     *
+     * @param args
      */
-    private static void startParallelThreads(){
-        QueuePressCommand.startCheckingQueue();
+    public static void main(String[] args){
+        launch(args);
     }
 
-    private static void checkRoles(List<Guild> guildList) {
-        for (Guild guild : guildList){
-            if(guild.getRolesByName(PLAYER, false).size() == 0) {
-                guild.createRole().setName(PLAYER).queue();
-            }
-            if(guild.getRolesByName(HOST, false).size() == 0) {
-                guild.createRole().setName(HOST).queue();
-            }
-        }
+
+    /**
+     * Essentially creates the entire GUI
+     * @param primaryStage the window that the GUI will exist in
+     * @throws Exception in case it is not possible to create the GUI.
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setOnCloseRequest(e -> System.exit (0));
+        primaryStage.setTitle("Discord Plays Launcher");
+
+        primaryStage.setMinWidth(400);
+        primaryStage.setMinHeight(200);
+        primaryStage.setMaxWidth(400);
+        primaryStage.setMaxHeight(200);
+
+        Scene scene = createScene();
+
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
     }
+
+    private Scene createScene() {
+        VBox vBox = createLayout();
+        return new Scene(vBox, 400, 200);
+    }
+
+    private VBox createLayout() {
+        VBox vBox = new VBox();
+        vBox.setPadding(new Insets(25, 25, 25, 25));
+
+        Text title = new Text("Welcome!");
+
+        Text description = new Text("Please enter the token that your bot will be using.");
+
+        Label token = new Label("Token: ");
+
+        TextField tokenField = new TextField();
+
+        HBox hBox = new HBox();
+        hBox.getChildren().addAll(token, tokenField);
+
+        Button startBot = new Button("Start Bot");
+
+        vBox.getChildren().addAll(title, description, hBox, startBot);
+
+        return vBox;
+    }
+
 }
