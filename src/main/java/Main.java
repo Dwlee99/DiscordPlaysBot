ec05353c710fce9021ce3222895b8253711d8908
Winston Osei-Bonsu <winstonobonsu@gmail.com>
2020-07-11 20:24:38 -0400

    Made roles for bot host and players.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,25 @@
 import message_handling.MessageHandler;
+import net.dv8tion.jda.api.entities.Guild;
 import reaction_handling.ReactionHandler;
 import commands.QueuePressCommand;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+import javax.xml.ws.Holder;
+import java.util.List;
+
 
 public class Main extends ListenerAdapter {
 
-    public static void main(String[] args) throws javax.security.auth.login.LoginException {
-        JDABuilder builder = JDABuilder.createDefault(args[0]);
-        JDA jda = builder.build();
+    private static final String PLAYER = "Discord Player";
+    private static final String HOST = "Game Host";
 
+    public static void main(String[] args) throws javax.security.auth.login.LoginException, InterruptedException {
+        JDABuilder builder = JDABuilder.createDefault(args[0]);
+        JDA jda = builder.build().awaitReady();
+
+        checkRoles(jda.getGuilds());
         jda.addEventListener(new MessageHandler());
         jda.addEventListener(new ReactionHandler());
 
@@ -23,5 +31,17 @@ public class Main extends ListenerAdapter {
      */
     private static void startParallelThreads(){
         QueuePressCommand.startCheckingQueue();
+    }
+
+    private static void checkRoles(List<Guild> guildList) {
+        for (Guild guild : guildList){
+            System.out.println(guild.getName());
+            if(guild.getRolesByName(PLAYER, false).size() == 0) {
+                guild.createRole().setName(PLAYER).queue();
+            }
+            if(guild.getRolesByName(HOST, false).size() == 0) {
+                guild.createRole().setName(HOST).queue();
+            }
+        }
     }
 }
